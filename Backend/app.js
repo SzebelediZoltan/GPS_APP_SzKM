5de5060b845520c79607d132625d5fdd921d0d1c
Kiss Dominik <kiss.dominik@ckik.hu>
2025-12-07 21:37:53 +0100

    https + pwa prep

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,9 @@
 const express = require("express");
 
 const app = express();
-
 const api = express();
 
 const cors = require("cors");
-
 const cookieParser = require("cookie-parser");
 
 app.use(cors(
--- Frontend/src/routes/__root.tsx
+++ Frontend/src/routes/__root.tsx
@@ -36,14 +36,47 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'TanStack Start Starter',
+        title: 'Vase',
       },
+      {
+        name: "description",
+        content: "Elképesztően elképesztő GPS"
+      },
+      {
+        name: "theme-color",
+        content: "#ffffff"
+      }
     ],
+
     links: [
       {
         rel: 'stylesheet',
         href: appCss,
       },
+      {
+        rel: "icon",
+        href: "/favicon.ico",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        href: "/apple-touch-icon.png",
+      },
+      {
+        rel: "manifest",
+        href: "/manifest.json"
+      }
     ],
   }),
 
