7bc81b1d6a208c83fbaa0adfad49922a82a1c8f5
Szebeledi Zoltan <szebe99@gmail.com>
2026-01-26 18:30:40 +0100

    wow

--- gpass-app-frontend/vite.config.ts
+++ gpass-app-frontend/vite.config.ts
@@ -18,7 +18,13 @@ export default defineConfig({
     tailwindcss(),
   ],
   server: {
-    port: 3000
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:4000",
+        changeOrigin: false
+      }
+    }
   },
   resolve: {
     alias: {
