4bc6c5a9f2fd2607f07d8e269c54fc1ee11069ee
SzebelédiZoltán <szebeledi.zoltan@ckik.hu>
2026-01-28 13:17:03 +0100

    local és nem local

--- gpass-app-backend/api/db/index.js
+++ gpass-app-backend/api/db/index.js
@@ -5,12 +5,12 @@ const { DbError } = require("../errors");
 const sequelize = new Sequelize
 (
     process.env.DB_NAME,
-    process.env.DB_USER || root,
+    process.env.DB_USER || "root",
     process.env.DB_PASSWORD || "",
 
     {
         dialect: process.env.DB_DIALECT,
-        host: process.env.DB_HOST || localhost,
+        host: process.env.DB_HOST || "localhost",
 
         logging: false,
     }
