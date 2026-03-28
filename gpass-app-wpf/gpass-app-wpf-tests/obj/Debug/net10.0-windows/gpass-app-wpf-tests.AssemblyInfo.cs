6732964e3e1adf612ea503d914ab4e440893231c
miskolczi.levente <mlevente05@gmail.com>
2026-03-28 10:23:02 -0700

    WPF setup update

--- gpass-app-wpf/gpass-app-wpf-tests/obj/Debug/net10.0-windows/gpass-app-wpf-tests.AssemblyInfo.cs
+++ gpass-app-wpf/gpass-app-wpf-tests/obj/Debug/net10.0-windows/gpass-app-wpf-tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gpass-app-wpf-tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7877e9e094510bde7b4af56ea13ae85e6d2fad20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("gpass-app-wpf-tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gpass-app-wpf-tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- gpass-app-wpf/gpass-app-wpf/obj/Debug/net10.0-windows/Views/Home.g.i.cs
+++ gpass-app-wpf/gpass-app-wpf/obj/Debug/net10.0-windows/Views/Home.g.i.cs
@@ -60,7 +60,7 @@ namespace gpass_app_wpf.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/gpass-app-wpf;component/views/home.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/gpass-app-wpf;V1.0.0.0;component/views/home.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\Home.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
